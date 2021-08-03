1606de4105f0c1e7f38259811b007a30024a0c63
Badal Yadav <ybadal88@gmail.com>
2021-08-02 21:02:11 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="logic_solver",
+    version="0.1",
+    description="Logic Solver",
+    url="https://github.com/badalyadav/logic_solver",
+    author_email="ybadal88@gmal.com",
+    license="MIT",
+    packages=["logic_solver"],
+    zip_safe=False,
+)
+
